8d22b5f7f8e6d9abd218fec5bb327acb05ae29e3
Arden Sentak <asentak@stevens.edu>
2025-04-23 23:33:22 -0400

    adding comments to page.tsx

--- src/frontend/app/page.tsx
+++ src/frontend/app/page.tsx
@@ -3,11 +3,12 @@ import { useState } from 'react';
 import { FaUpload } from 'react-icons/fa';
 
 export default function Home() {
-  const [image, setImage] = useState<File | null>(null);
-  const [isRecyclable, setIsRecyclable] = useState<string | null>(null);
-  const [imageUrl, setImageUrl] = useState<string | null>(null);
-  const [loading, setLoading] = useState(false);
+  const [image, setImage] = useState<File | null>(null); //will hold uploaded image files
+  const [isRecyclable, setIsRecyclable] = useState<string | null>(null); //holds prediction result
+  const [imageUrl, setImageUrl] = useState<string | null>(null); //will display image preview
+  const [loading, setLoading] = useState(false); //to signal that the image classifier is loading
 
+  //allows file input changes to occur + sends the files to backend for classification
   const handleFileChange = async (e: React.ChangeEvent<HTMLInputElement>) => {
     const file = e.target.files?.[0];
     if (file) {
@@ -33,10 +34,10 @@ export default function Home() {
       
         const data = await res.json();
         console.log(data); 
-        setIsRecyclable(data.prediction);
+        setIsRecyclable(data.prediction); //updates result with the prediction
       } catch (err) {
         console.error('Prediction error:', err);
-        setIsRecyclable('Error predicting');
+        setIsRecyclable('Error predicting'); //tells user if there was an error predicting
       } finally {
         setLoading(false);
       }
@@ -44,6 +45,7 @@ export default function Home() {
     }
   };
 
+  //text on right side of website
   const part1 = 'Recyclable or not?';
   const part2 = 'Upload an image now to find out.';
 
@@ -53,6 +55,12 @@ export default function Home() {
     });
   };
 
+  //website layout + style
+  /* BASIC SUMMARY: 
+  - 2 1/2 screen columns
+  - left: allows image to be uploaded, shows image, outputs prediction
+  - right: shows the text above
+  */
   return (
     <main className="h-screen flex">
       <div className="w-1/2 flex flex-col justify-center items-center bg-gray-100 p-6 space-y-4">
