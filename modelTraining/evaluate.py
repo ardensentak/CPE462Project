8bf28deaca437929f2af34a51f64be23d65b414e
Arden Sentak <asentak@stevens.edu>
2025-04-24 14:00:22 -0400

    add comments to evaluate.py

--- modelTraining/evaluate.py
+++ modelTraining/evaluate.py
@@ -1,10 +1,12 @@
+#Evaluates model performance based on the validation data
+
 import tensorflow as tf
 import numpy as np
 import matplotlib.pyplot as plt
 from sklearn.metrics import confusion_matrix, ConfusionMatrixDisplay, classification_report
 import os
 
-# Load model
+# Load trainined model
 model = tf.keras.models.load_model("models/recyclableClassifierFinalModel.keras")
 
 # Load validation dataset
@@ -14,30 +16,31 @@ val_ds = tf.keras.utils.image_dataset_from_directory(
     image_size=(224, 224),
     batch_size=32,
     label_mode='binary',
-    shuffle=False,  # Important for matching y_true and predictions
-    #class_names=["non_recyclable", "recyclable"]
+    shuffle=False,  
 )
 
-#preprocess same way as training
+#preprocess function to resize same as training script
+#but skipping data augmentation since its validation data
 def preprocess(image, label):
     image = image / 255.0
     image = (image - 0.5) * 2.0  # Normalize to [-1, 1]
     return image, label
 
-
+#preprocesses
 val_ds = val_ds.map(preprocess).prefetch(buffer_size=tf.data.AUTOTUNE)
 
-# Collect true labels and predictions
+# Will collect actual labels and prediction labels
 y_true = []
 y_pred = []
 
+#make predictions for all the images in the validation dataset
 for images, labels in val_ds:
-    preds = model.predict(images)
-    preds_binary = (preds > 0.5).astype(int).flatten()
-    y_true.extend(labels.numpy().astype(int))
-    y_pred.extend(preds_binary)
+    preds = model.predict(images) #predict is a built in function for Keras models
+    preds_binary = (preds > 0.5).astype(int).flatten() #0 = non_recyclable & 1 = recyclable
+    y_true.extend(labels.numpy().astype(int)) #adds actual label to true list
+    y_pred.extend(preds_binary) #adds predicted label to prediction list
 
-# Confusion Matrix
+# Generates a Confusion Matrix (uses sklearn.metrics)
 cm = confusion_matrix(y_true, y_pred)
 disp = ConfusionMatrixDisplay(confusion_matrix=cm, display_labels=['Non-Recyclable', 'Recyclable'])
 
@@ -46,6 +49,6 @@ disp.plot(cmap='Blues', values_format='d')
 plt.title("Confusion Matrix on Validation Set")
 plt.show()
 
-# Optional: Print classification metrics
+#Prints classification results
 print("\nClassification Report:")
 print(classification_report(y_true, y_pred, target_names=['Non-Recyclable', 'Recyclable']))
